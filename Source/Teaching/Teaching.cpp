51f4c10dd97d0bf12f2b96325aaa638b144721aa
Nino Filiu <filiunino@gmail.com>
2026-04-02 22:37:10 +0200

    empty template

---
+++ Source/Teaching/Teaching.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Teaching.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Teaching, "Teaching" );
